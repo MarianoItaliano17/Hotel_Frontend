75840a9fe2a1ddd604fe32ed1243a7f2fe6595e5
Marian <italianomariano198@gmail.com>
2024-12-02 17:49:03 -0300

    pagination created

--- src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../admin-services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
+  currentPage = 1;
+  rooms = [];
+  total:any;
+  loading = false;
 
+  constructor(
+    private adminService: AdminService,
+    private message: NzMessageService
+  ) {
+    this.getRooms();
+  }
+
+  getRooms(){
+    this.adminService.getRooms(this.currentPage -1).subscribe(res => {
+      console.log(res);
+      this.rooms = res.roomDtoList;
+      this.total = res.totalPages * 1;
+    })
+  }
+
+  pageIndexChange(value: any){
+    this.currentPage = value;
+    this.getRooms();
+  }
 }
