6062dbf424e119db9a4516c3061f07f43dab7a78
Mariano Italiano <86637016+MarianoItaliano17@users.noreply.github.com>
2024-12-09 17:57:10 -0300

    UpdateRoom page created

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -7,13 +7,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PostRoomComponent } from './components/post-room/post-room.component';
 import { DemoNgZorroAntdModule } from 'src/app/DemoNgZorroAntdModule';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UpdateRoomComponent } from './components/update-room/update-room.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     DashboardComponent,
-    PostRoomComponent
+    PostRoomComponent,
+    UpdateRoomComponent
   ],
   imports: [
     CommonModule,
